b1cf32fd9d8d7a688487af95c0f906d81fdb5ffb
KomaSeo <ysseo0910@gmail.com>
2024-05-04 19:53:53 +0900

    add task.js

---
+++ task.js
@@ -0,0 +1,11 @@
+
+
+function HandleTask(app,sqlServer){
+    app.post("./",async(req,res)=>{
+        const taskName = req.body.name;
+        const taskTime = new Date().toISOString().slice(0, 19).replace('T', ' ');;
+        const insertion_command = `insert into tasks value(${taskName}, ${taskTime})`
+        const res = await sqlServer.query(insertion_command)
+        res.status(202).send("complete");
+    })
+}
