a599c5205d3db3017f90f88ac974047366f5298d
KomaSeo <ysseo0910@gmail.com>
2024-06-08 23:25:16 +0900

    draft of delete and post API

--- task.js
+++ task.js
@@ -36,6 +36,7 @@ taskRouter.route("/").get((req,res)=>{
 
 })
 
+
 taskRouter.route("/:id").get(async (req,res)=>{
     const targetId = req.params.id;
     const query = client.query('SELECT * FROM tasks WHERE id=$1', [targetId]);
@@ -43,14 +44,45 @@ taskRouter.route("/:id").get(async (req,res)=>{
         if(queryResult.rows.length == 0){
             res.status(404).send(`Task with id ${targetId} not found.`);
         }
-        res.status(202).send(queryResult.rows[0])
+        else{
+            res.status(202).send(queryResult.rows[0])
+        }
     }).catch((reason)=>{
         console.log(reason);
         res.status(500).send("Unknown internal error found.");
     });
 }).post((req,res)=>{
+    const taskName = req.body["taskName"];
+    const taskTime = req.body["taskTime"];
+    if(taskName == null){
+        res.status(400).send("Property : taskName is not been sent.");
+    }
+    if(taskTime == null){
+        const currentDate = new Date();
+        taskTime = currentDate.toISOString();
+    }
+    const query = client.query('insert into tasks values (default, $1, $2)',[taskName, taskTime])
+    query.then((queryResult)=>{
+        res.status(202).send(queryResult.rows[0]);
+    }).catch((reason)=>{
+        console.log(reason);
+        req.status(500).send("Unknown internal error found");
+    })
 
 }).delete((req,res)=>{
+    const targetId = req.params.id;
+    const query = client.query('DELETE FROM tasks WHERE id=$1',[targetId]);
+    query.then((queryResult)=>{
+        if(queryResult.rows.length==0){
+            res.status(404).send(`Task with id ${targetId} not found.`);
+        }
+        else{
+            res.status(202).send(queryResult.rows[0]);
+        }
+    }).catch((reason)=>{
+        console.log(reason);
+        res.status(500).send("Unknown internal error found");
+    })
 
 }).put((req,res)=>{
 
