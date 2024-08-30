3d1e1f35535fb778facd8794abc2c07a69c58576
KomaSeo <ysseo0910@gmail.com>
2024-08-30 17:20:02 +0900

    change to strict typescript

--- passive_planning_front/src/index.tsx
+++ passive_planning_front/src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
-import reportWebVitals from './reportWebVitals.tsx';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 const rootContainer = document.getElementById('root')
 const rootDom = ReactDOM.createRoot(rootContainer ?? document.body);
 rootDom.render(
